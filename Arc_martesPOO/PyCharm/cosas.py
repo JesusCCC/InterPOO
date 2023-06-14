1f62bc8ec462034e9267eba371ff365729cbcb4d
JesusCCC <90366274+JesusCCC@users.noreply.github.com>
2023-06-13 21:10:45 -0600

    Add files via upload

---
+++ Arc_martesPOO/PyCharm/cosas.py
@@ -0,0 +1,7 @@
+class Alumno:
+    facultad = "FES Aragon"
+
+    def __init__(self, nom, ed, carr):
+        self.edad = ed
+        self.nombre = nom
+        self.carrera = carr
---
+++ Arc_martesPOO/PyCharm/main.py
@@ -0,0 +1,11 @@
+
+from arimetica import suma, resta
+
+def main():
+    resultado = suma(3,None)
+    print(resultado)
+    print(resta(b = 5, a = 3))
+    print("----Modificar atributos publicos----")
+
+
+main()
