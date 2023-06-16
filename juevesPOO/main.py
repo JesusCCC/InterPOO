3d9abd8c9edcafd8384d5d3e92e3fbd39c205f07
JesusCCC <90366274+JesusCCC@users.noreply.github.com>
2023-06-15 19:28:06 -0600

    Add files via upload

---
+++ juevesPOO/main.py
@@ -0,0 +1,15 @@
+from cosas import * #Libro, Autor, Alumno
+def main():
+    l1 = Libro.libro_planeta("El perfume", Autor("Patrick", "PS"), 1980)
+    print(l1)
+
+    l1.autor.pseudonimo = l1.autor.pseudonimo.lower()
+    print(l1)
+
+    print("-----------HERENCIA---------")
+    al2 = Alumno("Jose", 19, "23232", "ICO", 9)
+    al2.nombre = "Pepe"
+    print(vars(al2))
+
+
+main()
